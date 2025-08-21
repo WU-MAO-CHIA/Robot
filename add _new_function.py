eda44df2fe2b73c782a6d2d8a11424bae1d3cdad
mark <markwu0821@gamil.com>
2025-08-22 00:11:38 +0800

    master commit

--- add _new_function.py
+++ add _new_function.py
@@ -1,5 +1,9 @@
-import pandas as pd
-import numpy as np
+import os
+
+def test_function(var1):
+    print(f"aaa{var1}")
 
 if __name__ == "__main__":
+    a = 10
     print(f"Hello world")
+    test_function(a)
